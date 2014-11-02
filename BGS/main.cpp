adafbd76cf17e31287449e4e6b73f40a9fa7f104
Peter Henderson <Breakend@Peters-MacBook-Pro.local>
2014-11-02 13:18:08 -0500

    Initial Commit

---
+++ BGS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BGS
+//
+//  Created by Peter Henderson on 2014-11-02.
+//  Copyright (c) 2014 Peter Henderson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
